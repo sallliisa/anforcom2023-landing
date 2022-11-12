8daf876b7f4257981532216b65715e8e9ef4c305
salisa <sodiumcation@gmail.com>
2022-11-12 14:07:43 +0700

    gaming

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
+        <meta name='color-scheme' content='only light' />
+        <title>Anforcom 2022</title>
+        <link rel='icon' href='/favicon.ico' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,60 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { useState } from 'react'
+
+export default function Home() {
+  // create a useState for gridOne
+  return (
+    <div className={styles.container}>
+      <header className={styles.headerContainer}>
+        <div className={styles.header}>
+          <Image className={styles.logo} src={'/svgs/anforcom.svg'} alt='Anforcom 2022' width={64} height={64} />
+          <h1 className={styles.title}>Anforcom 2023</h1>
+          <p className={styles.description}>
+            Anforcom (Annual Informatics Competition) adalah serangkaian acara dan perlombaan yang diadakan Mahasiswa
+            Informatika Universitas Diponegoro. Event ini dilaksanakan dalam rangka mengenalkan Kultur Teknologi kepada
+            masyarakat dan menumbuhkan jiwa jiwa kompetitif yang sportif di kalangan pelajar.
+          </p>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <div className={styles.gridContainer}>
+          <div className={styles.gridOne}>
+            <div className={styles.stack}>
+              <h2 className={styles.gridTitle}>Tentang Anforcom</h2>
+              <p className={styles.gridDescription}>
+                Ketahui lebih lanjut tentang salah satu kompetisi computer science bergengsi di Indonesia
+              </p>
+            </div>
+          </div>
+          <div className={styles.gridTwo}>
+            <div className={styles.stack}>
+              <p className={styles.gridDescription}>Masuk ke Anforcom</p>
+              <h2 className={styles.gridTitle}>Dashboard</h2>
+            </div>
+          </div>
+          <div className={styles.gridThree}>
+            <div className={styles.stack}>
+              <h2 className={styles.gridTitle}>Diponegoro UI/UX Competition</h2>
+              <p className={styles.gridDescription}>Revolt, Be Creative, Be Different!</p>
+            </div>
+          </div>
+          <div className={styles.gridFour}>
+            <div className={styles.stack}>
+              <h2 className={styles.gridTitle}>Diponegoro Logic Competition</h2>
+              <p className={styles.gridDescription}>Breaking the logic gate</p>
+            </div>
+          </div>
+          <div className={styles.gridFive}>
+            <div className={styles.stack}>
+              <h1>Awake</h1>
+              <p>How to Find Your Passion on Information Technology World</p>
+            </div>
+            <h2 className={styles.gridTitle}>Seminar</h2>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
